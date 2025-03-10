c374a1a862b9b712f4ecfb71b7782fbc47b5f991
Hussam ElNemr <hassom20042019@gmail.com>
2025-03-11 00:51:01 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,5 +4,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
-movie_response = requests.get(f"https://api.themoviedb.org/3/movie/640?api_key={TMDB_API_KEY}")
-print(movie_response.text)
+headers = {
+    "accept": "application/json"
+}
+url = f"https://api.themoviedb.org/3/movie/640?api_key={TMDB_API_KEY}"
+movie_response = requests.get(url, headers=headers)
+print(f"{movie_response.text}\n\n\n\n\n\n")
+if movie_response.status_code == 200:
+    movie_data = movie_response.json()
+    name = movie_data.get("title")
+    genres = [{"id": genre["id"], "name": genre["name"]} for genre in movie_data.get("genres", [])]
+    
+    filtered_response = {
+        "name": name,
+        "genres": genres
+    }
+    
+    print(filtered_response)
+else:
+    print(f"Error: {movie_response.status_code}")

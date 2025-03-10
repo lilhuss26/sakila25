6ee8c6c3d832a73ab9957ae26ea78cf210abf25e
Hussam ElNemr <hassom20042019@gmail.com>
2025-03-11 00:27:27 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,4 +4,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
-print(TMDB_API_KEY)
+movie_response = requests.get(f"https://api.themoviedb.org/3/movie/640?api_key={TMDB_API_KEY}")
+print(movie_response.text)

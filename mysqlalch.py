11ae20363535e968cf12a622329fb6f5405ba5f1
Hussam ElNemr <hassom20042019@gmail.com>
2025-03-12 00:11:47 +0200

    connect to mysql laptop

---
+++ mysqlalch.py
@@ -0,0 +1,22 @@
+import os
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.declarative import declarative_base
+
+load_dotenv()
+connection_string = os.getenv('MYSQL_STRING')
+
+engine = create_engine(connection_string)
+Base = automap_base()
+
+Base.prepare(autoload_with=engine)
+LaptopPrices = Base.classes.laptop_prices
+
+session = Session(engine)
+
+laptops = session.query(LaptopPrices).all()
+for laptop in laptops:
+    print(laptop.__dict__)
+session.close()

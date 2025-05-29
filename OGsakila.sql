d4c1631826164437bd07715c93adc510297cb88d
Hussam ElNemr <hassom20042019@gmail.com>
2025-05-29 16:42:22 +0300

    Top rated init

---
+++ OGsakila.sql
@@ -0,0 +1,4 @@
+-- Active: 1741719431166@@127.0.0.1@3306@sakila
+USE sakila;
+
+SELECT * from film ;
--- TopRated.py
+++ TopRated.py
@@ -9,22 +9,27 @@ sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 load_dotenv()
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 
-topurl = f"https://api.themoviedb.org/3/movie/top_rated?api_key={TMDB_API_KEY}&page=1&language=en-US"
-topRated = requests.get(url=topurl)
+top_ratedURL = f"https://api.themoviedb.org/3/movie/top_rated?api_key={TMDB_API_KEY}&page=1&language=en-US"
+topRated = requests.get(url=top_ratedURL)
 topRated_data = topRated.json()
 results = topRated_data.get("results")
-movie_rate = {}
+topRated_movies = {}
 for movie in results:
     film_id = movie["id"]
     title = movie["title"]
-    rate = movie["vote_average"]
-    movie_rate[film_id] = {
+    description = movie["overview"]
+    release_date = movie["release_date"]
+    language = movie["original_language"]
+    rating = movie["vote_average"]
+    topRated_movies[film_id] = {
         #"id":film_id,
         "title":title,
-        "rate":rate
+        "rating":rating,
+        "description":description,
+        "description":description,
+        "language":language,
     }
-#print(movie_rate)
-movie_rate_df = pd.DataFrame.from_dict(movie_rate, orient='index')
+movie_rate_df = pd.DataFrame.from_dict(topRated_movies, orient='index')
 movie_rate_df = movie_rate_df.rename_axis('id')
 movie_rate_df = movie_rate_df.reset_index()
 movie_rate_df.to_csv("top_rated_movies.csv", index=False)

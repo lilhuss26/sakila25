98f85a524039be6f639320c33fabf3cb9d524bf5
Hussam ElNemr <hassom20042019@gmail.com>
2025-09-01 23:53:15 +0300

    Providers

--- PreAPI.py
+++ PreAPI.py
@@ -9,3 +9,7 @@ sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 load_dotenv()
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 
+base_link = f"https://api.themoviedb.org/3/watch/providers/movie?api_key={TMDB_API_KEY}&language=en-US&watch_region=US"
+providers = requests.get(url=base_link)
+providers_data = providers.json()
+print(providers_data)
